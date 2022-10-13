e77cb7c9f5c4e7e293f1bf23c43a4fe81fd4b3eb
julianboyne <julianboyne11@gmail.com>
2022-10-13 18:39:45 -0400

    display my basic info

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <Route path='/' element={<Home />}></Route>
     <Route path='/contact' element={<Contact />}></Route>
     <Route path='/projects' element={<Projects />}></Route>
+    <Route path="/resume" element={<Resume />}></Route>
   </Routes>
   </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import "../css pages/Home.css"
+
 const Home = () => {
   return (
-  <>
-    <h1>Home Page</h1>
-  </>
-  );
+  <div className="home-page">
+    <h1>Julian Boyne</h1>
+    <div className="img">
+      <img id="me" src="./assets/yo.png" alt="Julian" />
+    </div>
+    <h3>Software Engineer</h3>
+  </div>
+  )
 }
 
 export default Home;
