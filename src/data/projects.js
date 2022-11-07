30b2387b7ee54c071ec220ffa07e5094be8cb649
julianboyne <julianboyne11@gmail.com>
2022-11-06 19:43:27 -0500

    implement projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,28 @@
+import "../css pages/Project.css"
+
 const projects = [
   {
     title: "Blackjack",
     description: "Game goal is to beat dealer by being closer to 21 than them. if you pass 21, YOU LOSE.",
-    image:"https://github.com/julianboyne11/BlackJack/blob/main/images/assets/screenshot/front%20game.png",
+    image: "https://github.com/julianboyne11/BlackJack/raw/main/images/assets/screenshot/front%20game.png",
     repositoryLink: "https://github.com/julianboyne11/BlackJack",
     deploymentLink: "https://julian-boyne-blackjack.netlify.app/"
   },
   {
     title: "Game Valley",
     description: "Games collection app where you can add games, comment and see other peoples games and profile.",
-    image: "https://github.com/julianboyne11/game-valley/blob/main/public/assets/screenshots/Screenshot%20(30).png",
+    image: "https://github.com/julianboyne11/game-valley/raw/main/public/assets/screenshots/Screenshot%20(30).png",
     repositoryLink: "https://github.com/julianboyne11/game-valley",
     deploymentLink: "https://game-valley.fly.dev/"
   },
   {
     title: "myProps",
     description: "myProps is a website built for property managers to easily keep track of properties that they manage. myProps goal is to keep information private to the user, so nobody else has access to their information. Accessibility is key as well, as we want our users to get a feel for the website and make navigation easygoing for their experience.",
-    image: "https://github.com/julianboyne11/myProps-front-end/blob/main/public/landingpage.jpg",
-    frontRepositoryLink: "https://github.com/julianboyne11/myProps-front-end",
-    backRepositoryLink: "https://github.com/julianboyne11/myProps-back-end",
+    image: "https://github.com/julianboyne11/myProps-front-end/raw/main/public/landingpage.jpg",
+    repositoryLink: "https://github.com/julianboyne11/myProps-front-end",
     deploymentLink: "https://mypropsmanagement.netlify.app/",
     groupMembers: ["Tal Rimoni", "Chris Mems", "Mike Trinh", "Julian Boyne"]
   },
 ]
+
+export {projects}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,23 @@
+import { projects } from "../data/projects";
+
 const Projects = () => {
   return (
     <>
       <h1>My Projects</h1>
+      {projects.map(project => (
+        <ul>
+          <h2>
+            {project.title}
+          </h2>
+          <img src={project.image} alt="App" />
+          <p>{project.description}</p>
+          <a href={project.repositoryLink}>Git Hub</a>
+          <a href={project.deploymentLink}>App</a>
+          {project.groupMembers?.map(member => (
+            <p>{member}</p>
+          ))}
+        </ul>
+      ))}
     </>
   );
 }
