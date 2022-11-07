dde4dfb1c9bd80a8e1f8fc86a6b1164b46db24d7
julianboyne <julianboyne11@gmail.com>
2022-11-07 08:48:58 -0500

    add contact info to page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
+import "../css pages/Contact.css"
+
 const Contact = () => {
   return ( 
-    <>
-      <h1>Contact Me</h1>
-    </>
+    <div>
+      <h2>Reach out if you want to work with me! You can contact me via:</h2>
+      <a href="https://github.com/julianboyne11"><img className="icon"  src="./assets/GitHub-Mark-Light-64px.png" alt="GitHub" /></a>
+      <a href="https://www.linkedin.com/in/julian-boyne/"><img className="icon" src="./assets/linkedin.svg" alt="Linkedin" /></a>
+      <a href="mailto:julianboyne11@gmail.com"><img className="icon" src="./assets/mail.svg" alt="" /></a>
+    
+    </div>
   );
 }
 
