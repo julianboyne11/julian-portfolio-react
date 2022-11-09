5e6ddf8f35b913ebc12aab608ab4421f328a4aa2
julianboyne <julianboyne11@gmail.com>
2022-11-09 14:11:27 -0500

    ch back-color of github icon

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <div className="contact">
       <h2>Reach out if you want to work with me! You can contact me via:</h2>
       <div>
-      <a href="https://github.com/julianboyne11"><img className="icon"  src="./assets/GitHub-Mark-Light-64px.png" alt="GitHub" /></a>
-      <a href="https://www.linkedin.com/in/julian-boyne/"><img className="icon" src="./assets/linkedin.svg" alt="Linkedin" /></a>
+      <a href="https://github.com/julianboyne11"><img className="icon" id="github"  src="./assets/GitHub-Mark-Light-64px.png" alt="GitHub" /></a>
+      <a href="https://www.linkedin.com/in/julian-boyne/" ><img className="icon"  src="./assets/linkedin.svg" alt="Linkedin" /></a>
       <a href="mailto:julianboyne11@gmail.com"><img className="icon" src="./assets/mail.svg" alt="" /></a>
       </div>
     </div>
