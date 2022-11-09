bbe054d1c087f5e11d4c85c58cba9bf74402f3a6
julianboyne <julianboyne11@gmail.com>
2022-11-09 13:14:06 -0500

    minor css on the home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,8 @@ const About = () => {
   return ( 
     <>
     <div className="about-me-container">
-      <h1>About Me</h1>
+      <h1 className="title">About Me</h1>
+      <br /><br />
         <img id="me" src="./assets/yo.png" alt="Julian" /><br />
       <hr />
       <h3>A food and beverage service provider that turned around to a full stack developer. Who works great in teams by being the example and motivating not only myself but the whole crew along with communicating effectively and adapting to any type of situation.</h3>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,12 +6,12 @@ const Projects = () => {
   return (
     <>
       <h1>My Projects</h1>
+      <hr className="title"/><br /><br />
       <div className=" project-container">
       {projects.map(project => (
         <Project key={project.title} project={project} />
       ))}
       </div>
-      {/* <Contact /> */}
     </>
   );
 }
