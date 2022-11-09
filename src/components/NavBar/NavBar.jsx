e47ea32cc6de234b50234d1275bd987a79d37562
julianboyne <julianboyne11@gmail.com>
2022-11-09 14:28:46 -0500

    implement minor mobile responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
           <Link className="App-link" to="/">Julian Boyne</Link>
           <Link className="App-link" to="/about">About Me</Link>
           <Link className="App-link" to="/projects">My Work</Link>
-          <Link className="App-link" to="/contact">Contact Me</Link>
+          <Link className="App-link" to="/contact" id="contact">Contact Me</Link>
         </nav>
       </header>
     )
