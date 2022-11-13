9a201b3f3cb1f9048be4d79235300c679f6ec3f1
julianboyne <julianboyne11@gmail.com>
2022-11-13 09:02:52 -0500

    add skills in about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,6 +10,10 @@ const About = () => {
         <img id="me" src="./assets/yo.png" alt="Julian" /><br />
       <hr />
       <h3>I'm a full stack developer with experience in the food and beverage industry. I work well in teams by being adaptable, leading by example, motivating myself and others, and communicating effectively. When building apps, I get to exercise envisioning all possible scenarios to tackle problems that I'm solving with all projects.</h3>
+      <h2>Skills</h2>
+      <hr />
+      <h3>HTML - CSS (Cascading Style Sheets) - JavaScript - Python - Mongoose - Reactjs - Express - Django - PostgresSQL - MongoDB - Nodejs</h3>
+      <br /><br /><br />
     </div>
     </>
   )
